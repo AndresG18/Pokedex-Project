061601e70ad29ce3997480bc04b3a2f649f86629
Chris Kang <chriskangfamily@gmail.com>
2024-05-16 11:09:37 -0700

    Added Types route in pokemon

--- flask-backend/app/routes/pokemon.py
+++ flask-backend/app/routes/pokemon.py
@@ -1,3 +1,27 @@
 from flask import Blueprint
 
+
 bp = Blueprint("pokemon", __name__, url_prefix="/api/pokemon")
+
+types = [
+  "fire",
+  "electric",
+  "normal",
+  "ghost",
+  "psychic",
+  "water",
+  "bug",
+  "dragon",
+  "grass",
+  "fighting",
+  "ice",
+  "flying",
+  "poison",
+  "ground",
+  "rock",
+  "steel",
+];
+
+@bp.route("/types")
+def getTypes():
+  return { "types": types }

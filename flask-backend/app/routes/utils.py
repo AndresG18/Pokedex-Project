1e90c32d895273203d01f93646b268948e0cbd90
Daniel <daniel.choi91011@gmail.com>
2024-05-16 15:27:15 -0700

    utils refactored

---
+++ flask-backend/app/routes/utils.py
@@ -0,0 +1,12 @@
+import random
+
+def random_item_images():
+    images = [
+        "/images/pokemon_berry.svg",
+        "/images/pokemon_egg.svg",
+        "/images/pokemon_potion.svg",
+        "/images/pokemon_super_potion.svg",
+    ]
+    
+    i = random.randint(0, len(images) - 1)
+    return images[i]
